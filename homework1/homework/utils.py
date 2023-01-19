0df55999a098ad88966c8f74c3f35a092e416339
Baktanoosh <77557980+Baktanoosh@users.noreply.github.com>
2023-01-18 21:10:39 -0500

    Update utils.py

--- homework1/homework/utils.py
+++ homework1/homework/utils.py
@@ -1,33 +1,43 @@
 from PIL import Image
-
+import torch
+import csv
+from os import path
+import torchvision
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 
 LABEL_NAMES = ['background', 'kart', 'pickup', 'nitro', 'bomb', 'projectile']
 
 
-class SuperTuxDataset(Dataset):
-    def __init__(self, dataset_path):
-        """
-        Your code here
-        Hint: Use the python csv library to parse labels.csv
 
-        WARNING: Do not perform data normalization here. 
-        """
-        raise NotImplementedError('SuperTuxDataset.__init__')
+class SuperTuxDataset(Dataset):
+    
+    def __init__(self, dataset_path):
+        self.init_tensor = torchvision.transforms.ToTensor()
+        with open(dataset_path+"/"+"labels.csv") as labels_csv_file:
+            label_reader = csv.reader(labels_csv_file)
+            next(label_reader) 
+            self.input_data = []
+            for row in label_reader:
+                file_name = row[0]
+                label = row[1]
+                j = 0
+                for i in LABEL_NAMES:
+                  if  label == i:
+                    label_index = j
+                    break
+                  j += 1
+                image_tensor = self.init_tensor(Image.open(dataset_path+"/"+file_name))
+                self.input_data.append((image_tensor,label_index))
+
 
     def __len__(self):
-        """
-        Your code here
-        """
-        raise NotImplementedError('SuperTuxDataset.__len__')
+      self.length = len(self.input_data)
+      return self.length
 
     def __getitem__(self, idx):
-        """
-        Your code here
-        return a tuple: img, label
-        """
-        raise NotImplementedError('SuperTuxDataset.__getitem__')
+      self.item = self.input_data[idx]
+      return self.item
 
 
 def load_data(dataset_path, num_workers=0, batch_size=128):
@@ -38,3 +48,4 @@ def load_data(dataset_path, num_workers=0, batch_size=128):
 def accuracy(outputs, labels):
     outputs_idx = outputs.max(1)[1].type_as(labels)
     return outputs_idx.eq(labels).float().mean()
+
