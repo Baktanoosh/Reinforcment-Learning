3a1ca8fcf7c6a0ea0751a6a933af33edb84b8344
Baktanoosh <77557980+Baktanoosh@users.noreply.github.com>
2023-01-29 23:34:03 -0500

    Update train.py

--- homework2/homework/train.py
+++ homework2/homework/train.py
@@ -11,12 +11,44 @@ def train(args):
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
         valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
+        
+    train_path = "/content/cs342/homework2/data/train"
+    valid_path = "/content/cs342/homework2/data/valid"
+    n_epochs = int(args.epochs)
+    batch_size = int(args.batch_size)
+    lr = float(args.learning_rate)
+    if torch.cuda.is_available(): device=torch.device('cuda')
+    else: device=torch.device('cpu')
+    print('device = ', device)
+    model.to(device)
+    model.train()
+    optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
+    loss = torch.nn.CrossEntropyLoss()
+    train_dataloader = load_data(train_path, num_workers=0, batch_size=batch_size)
+    valid_dataloader = load_data(valid_path, num_workers=0, batch_size=batch_size)
 
-    """
-    Your code here, modify your HW1 code
-    
-    """
+    for epoch in range(n_epochs):
+        train_accuracy = []
+        for train_features, train_labels in train_dataloader:
+            train_features,train_labels = train_features.to(device),train_labels.to(device)
+            o = model(train_features)
+            loss_val = loss(o, train_labels)
+            train_accuracy.append((accuracy(o,train_labels).cpu().detach().numpy()))
+            optimizer.zero_grad()
+            loss_val.backward()
+            optimizer.step()
+        valid_features, valid_labels = next(iter(valid_dataloader))
+        valid_features, valid_labels = valid_features.to(device), valid_labels.to(device)
+        valid_pred = model(valid_features)
+        valid_accuracy = accuracy(valid_pred, valid_labels)
 
+        print("-----------------------------------------------------------------------------")
+        print("EPOCH: %s"%epoch)
+        print("Train Accuracy: %s" % np.mean(train_accuracy))
+        print("Validation Accuracy: %s" % valid_accuracy)
+        print("-----------------------------------------------------------------------------")
+
+    print("saving model to disk")
     save_model(model)
 
 
