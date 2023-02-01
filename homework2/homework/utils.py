5cb42719441daecd07bf4118db7eb2613a880ffb
Baktanoosh <77557980+Baktanoosh@users.noreply.github.com>
2023-01-31 23:48:10 -0500

    Update utils.py

--- homework2/homework/utils.py
+++ homework2/homework/utils.py
@@ -10,24 +10,30 @@ class SuperTuxDataset(Dataset):
     WARNING: Do not perform data normalization here. 
     """
     def __init__(self, dataset_path):
-        """
-        Your code here
-        Hint: Use your solution (or the master solution) to HW1
-        """
-        raise NotImplementedError('SuperTuxDataset.__init__')
-
+        self.init_tensor = torchvision.transforms.ToTensor()
+        with open(dataset_path+"/"+"labels.csv") as labels_csv_file:
+            label_reader = csv.reader(labels_csv_file)
+            next(label_reader) 
+            self.input_data = []
+            for row in label_reader:
+                file_name = row[0]
+                label = row[1]
+                j = 0
+                for i in LABEL_NAMES:
+                  if  label == i:
+                    label_index = j
+                    break
+                  j += 1
+                image_tensor = self.init_tensor(Image.open(dataset_path+"/"+file_name))
+                self.input_data.append((image_tensor,label_index))
     def __len__(self):
-        """
-        Your code here
-        """
-        raise NotImplementedError('SuperTuxDataset.__len__')
+        self.length = len(self.input_data)
+        return self.length
+
 
     def __getitem__(self, idx):
-        """
-        Your code here
-        return a tuple: img, label
-        """
-        raise NotImplementedError('SuperTuxDataset.__getitem__')
+        self.item = self.input_data[idx]
+        return self.item
 
 
 def load_data(dataset_path, num_workers=0, batch_size=128):
