778d0b896531a54a8b2d5cde8d0067f99ab22e80
Baktanoosh <77557980+Baktanoosh@users.noreply.github.com>
2023-02-02 01:25:38 -0500

    Update train.py

--- homework2/homework/train.py
+++ homework2/homework/train.py
@@ -2,54 +2,50 @@ from .models import CNNClassifier, save_model
 from .utils import accuracy, load_data
 import torch
 import torch.utils.tensorboard as tb
+import numpy as np
+
 
 
 def train(args):
-    from os import path
-    model = CNNClassifier()
-    train_logger, valid_logger = None, None
-    if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
-        valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'))
-        
     train_path = "/content/cs342/homework2/data/train"
     valid_path = "/content/cs342/homework2/data/valid"
-    n_epochs = int(args.epochs)
-    batch_size = int(args.batch_size)
-    lr = float(args.learning_rate)
-    if torch.cuda.is_available(): device=torch.device('cuda')
-    else: device=torch.device('cpu')
+    
+    num_epochs = 100
+    batch_size = 64
+    learning_rate = 0.001
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
     print('device = ', device)
+    model = CNNClassifier()
     model.to(device)
-    model.train()
-    optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
-    loss = torch.nn.CrossEntropyLoss()
-    train_dataloader = load_data(train_path, num_workers=0, batch_size=batch_size)
-    valid_dataloader = load_data(valid_path, num_workers=0, batch_size=batch_size)
-
-    for epoch in range(n_epochs):
+    train_data = load_data(train_path)
+    valid_data = load_data(valid_path)
+    loss = torch.nn.CrossEntropyLoss()   
+    
+    global_step = 0
+    for epoch in range(num_epochs):
         train_accuracy = []
-        for train_features, train_labels in train_dataloader:
-            train_features,train_labels = train_features.to(device),train_labels.to(device)
-            o = model(train_features)
-            loss_val = loss(o, train_labels)
-            train_accuracy.append((accuracy(o,train_labels).cpu().detach().numpy()))
+        train_accuracy_value = []
+        train_loss = []
+        train_loss_value = []
+        valid_accuracy = []
+        valid_accuracy_value = []
+
+        optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate , momentum=0.9, weight_decay=1e-4)
+        for i, (data, labels) in enumerate(train_data):
+            o = model(data.to(device))
+            train_loss = loss(o, labels.to(device))
+            train_loss_value.append(train_loss.float().detach().cpu().numpy())
+            train_accuracy = accuracy(o,labels)
+            train_accuracy_value.append(train_accuracy.cpu().detach().numpy())
             optimizer.zero_grad()
-            loss_val.backward()
+            train_loss.backward()
             optimizer.step()
-        valid_features, valid_labels = next(iter(valid_dataloader))
-        valid_features, valid_labels = valid_features.to(device), valid_labels.to(device)
-        valid_pred = model(valid_features)
-        valid_accuracy = accuracy(valid_pred, valid_labels)
+            global_step += 1
 
-        print("-----------------------------------------------------------------------------")
-        print("EPOCH: %s"%epoch)
-        print("Train Accuracy: %s" % np.mean(train_accuracy))
-        print("Validation Accuracy: %s" % valid_accuracy)
-        print("-----------------------------------------------------------------------------")
+            print (f'Epoch - {global_step+1}, Loss - {round(train_loss.item(),3)}')
+        print("------------------------------------------------------------")
+        save_model(model)
 
-    print("saving model to disk")
-    save_model(model)
 
 
 if __name__ == '__main__':
