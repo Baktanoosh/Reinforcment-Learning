ec025ccb374fd3b606c89a2f8c2df083357742d5
Baktanoosh <77557980+Baktanoosh@users.noreply.github.com>
2023-02-22 21:02:11 -0500

    Update models.py

--- homework3/homework/models.py
+++ homework3/homework/models.py
@@ -3,15 +3,25 @@ import torch.nn.functional as F
 
 
 class CNNClassifier(torch.nn.Module):
-    def __init__(self):
+    def __init__(self, in_put, out_put, stride=1):
         super().__init__()
         """
         Your code here
         Hint: Base this on yours or HW2 master solution if you'd like.
         Hint: Overall model can be similar to HW2, but you likely need some architecture changes (e.g. ResNets)
         """
-        raise NotImplementedError('CNNClassifier.__init__')
+        self.layers = torch.nn.Sequential(
+                  torch.nn.BatchNorm2d(in_put),
+                  torch.nn.Conv2d(in_put, out_put, 3, stride=stride, padding=1, bias=False),
+                  torch.nn.ReLU(),
+                  torch.nn.BatchNorm2d(out_put),
+                  torch.nn.Conv2d(out_put, out_put, 3, padding=1)
+                )
+        self.identity = torch.nn.Identity()
+        if c_in != c_out or stride != 1:
+            self.identity = torch.nn.Conv2d(in_put, out_put, 1, stride=stride)
 
+        
     def forward(self, x):
         """
         Your code here
@@ -19,7 +29,9 @@ class CNNClassifier(torch.nn.Module):
         @return: torch.Tensor((B,6))
         Hint: Apply input normalization inside the network, to make sure it is applied in the grader
         """
-        raise NotImplementedError('CNNClassifier.forward')
+        x_in = x
+        x = self.layers(x)
+        return x + self.identity(x_in)
 
 
 class FCN(torch.nn.Module):
