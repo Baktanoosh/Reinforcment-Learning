3ba5c33b06562fb1abe89e817f2cf094fd9387ac
Baktanoosh <77557980+Baktanoosh@users.noreply.github.com>
2023-03-19 22:47:13 -0400

    Update train.py

--- homework4/homework/train.py
+++ homework4/homework/train.py
@@ -1,7 +1,7 @@
 import torch
 import numpy as np
-import torch
 import inspect
+import torchvision.transforms as trans
 from .models import Detector, save_model
 from .utils import load_detection_data
 from . import dense_transforms
@@ -10,69 +10,39 @@ import torch.utils.tensorboard as tb
 
 def train(args):
     from os import path
-    model = Detector()
     train_logger, valid_logger = None, None
     if args.log_dir is not None:
         train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'), flush_secs=1)
         valid_logger = tb.SummaryWriter(path.join(args.log_dir, 'valid'), flush_secs=1)
 
-    num_epochs = 20
-    learning_rate = 0.001
+    """
+    Your code here, modify your HW3 code
+    Hint: Use the log function below to debug and visualize your model
+    """
+    global_step = 0
+    num_epoch = 20
+    learning_rate = 1e-3 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    print('device = ', device)
-    model.to(device)
-    if args.continue_training:
-        model.load_state_dict(torch.load(path.join(path.dirname(path.abspath(__file__)), 'det.th')))
-    transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
-    train_data = load_dense_data('dense_data/train', num_workers=4)
-    train_data_transformed = load_dense_data('dense_data/train', num_workers=4, transform=transform)
-    valid_data = load_dense_data('dense_data/valid', num_workers=4)
+    model = Detector().to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
     loss = torch.nn.BCEWithLogitsLoss().to(device)
-    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
-    global_step = 0
-
-    for epoch in range(num_epochs):
-        train_accuracy = []
-        train_accuracy_value = []
+    transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
+    train_data = load_detection_data('dense_data/train', num_workers=4, transform=transform)
+    for epoch in range(num_epoch):
         model.train()
-        confusion_matrix = ConfusionMatrix()
-        for image, label in train_data:
-            image = image.to(device)
-            label = torch.tensor(label, dtype=torch.long, device=device)
-            pred = model(image)
-            loss_val = loss(pred, label)
+        for img, label,_ in train_data:
+            img, label = img.to(device), label.to(device)
+            logit = model(img)
+            loss_val = loss(logit, label)
             optimizer.zero_grad()
             loss_val.backward()
             optimizer.step()
-            train_accuracy = accuracy(o,labels)
-            train_accuracy_value.append(train_accuracy.cpu().detach().numpy())
             global_step += 1
-        for image, label in train_data_transformed:
-            image = image.to(device)
-            label = torch.tensor(label, dtype=torch.long, device=device)
-            pred = model(image)
-            loss_val = loss(pred, label)
-            optimizer.zero_grad()
-            loss_val.backward()
-            optimizer.step()
-            train_accuracy = accuracy(o,labels)
-            train_accuracy_value.append(train_accuracy.cpu().detach().numpy())
-            global_step += 1
-        model.eval()
-        total_step = 0
-        accuracy = 0
-        for i, (image, label) in enumerate(valid_data):
-            image, label = image.to(device), label.to(device)
-            pred = model(image)
-            accuracy = accuracy + (pred.argmax(1) == label).float().mean().item()
-            total_step += 1
-            print("------------------------------------------------------------")
-            print("Epoch: " + str(epoch+1))
-            print("Accuracy: " + "{0:.3f}".format(accuracy/total_step))
-        scheduler.step(loss_val)
+        print("------------------------------------------------------------")
+        print("Epoch: " + str(epoch+1))
+        print("Loss: ",loss_val)   
     save_model(model)
- 
+
 
 def log(logger, imgs, gt_det, det, global_step):
     """
@@ -93,6 +63,6 @@ if __name__ == '__main__':
 
     parser.add_argument('--log_dir')
     # Put custom arguments here
-
+    parser.add_argument('-t', '--transform', default='Compose([ColorJitter(0.9, 0.9, 0.9, 0.1), RandomHorizontalFlip(), ToTensor(), ToHeatmap()])')
     args = parser.parse_args()
     train(args)
