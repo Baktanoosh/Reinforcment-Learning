7002f4535e524564c7bf494e9af259bcc3263679
Baktanoosh <77557980+Baktanoosh@users.noreply.github.com>
2023-04-02 21:42:08 -0400

    Update planner.py

--- homework5/homework/planner.py
+++ homework5/homework/planner.py
@@ -38,7 +38,7 @@ class Planner(torch.nn.Module):
         def forward(self, x):
             return F.relu(self.c1(x))
 
-    def __init__(self, layers=[16, 32, 64, 128], n_class=3, kernel_size=3, use_skip=True):
+    def __init__(self, layers=[16, 32, 64, 128], n_class=1, kernel_size=3, use_skip=True):
 
         super().__init__()
         self.input_mean = torch.Tensor([0.2788, 0.2657, 0.2629])
