dc96180425de651a0d837c9605231867e6b0a060
Baktanoosh <77557980+Baktanoosh@users.noreply.github.com>
2023-04-04 22:05:39 -0400

    Update planner.py

--- homework5/homework/planner.py
+++ homework5/homework/planner.py
@@ -15,69 +15,69 @@ def spatial_argmax(logit):
 
 class Planner(torch.nn.Module):
     class Block(torch.nn.Module):
-        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
-            super().__init__()
-            self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                      stride=stride)
-            self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
-            self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2)
-            self.b1 = torch.nn.BatchNorm2d(n_output)
-            self.b2 = torch.nn.BatchNorm2d(n_output)
-            self.b3 = torch.nn.BatchNorm2d(n_output)
-            self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
+      def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+          super().__init__()
+          self.c1 = torch.nn.Conv2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                    stride=stride, bias=False)
+          self.c2 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+          self.c3 = torch.nn.Conv2d(n_output, n_output, kernel_size=kernel_size, padding=kernel_size // 2, bias=False)
+          self.b1 = torch.nn.BatchNorm2d(n_output)
+          self.b2 = torch.nn.BatchNorm2d(n_output)
+          self.b3 = torch.nn.BatchNorm2d(n_output)
+          self.skip = torch.nn.Conv2d(n_input, n_output, kernel_size=1, stride=stride)
 
-        def forward(self, x):
-            return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
-
+      def forward(self, x):
+          return F.relu(self.b3(self.c3(F.relu(self.b2(self.c2(F.relu(self.b1(self.c1(x)))))))) + self.skip(x))
+    
     class UpBlock(torch.nn.Module):
-        def __init__(self, n_input, n_output, kernel_size=3, stride=2):
-            super().__init__()
-            self.c1 = torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
-                                               stride=stride, output_padding=1)
+      def __init__(self, n_input, n_output, kernel_size=3, stride=2):
+          super().__init__()
+          self.c1 = torch.nn.ConvTranspose2d(n_input, n_output, kernel_size=kernel_size, padding=kernel_size // 2,
+                                    stride=stride, output_padding=1)
 
-        def forward(self, x):
-            return F.relu(self.c1(x))
+      def forward(self, x):
+          return F.relu(self.c1(x))
 
-    def __init__(self, layers=[16, 32, 64, 128], n_class=1, kernel_size=3, use_skip=True):
+    def __init__(self, layers=[16, 32, 64, 128], n_output_channels=1, kernel_size=3, use_skip=True):
+      super().__init__()
+      self.input_mean = torch.Tensor([0.2788, 0.2657, 0.2629])
+      self.input_std = torch.Tensor([0.2064, 0.1944, 0.2252])
 
-        super().__init__()
-        self.input_mean = torch.Tensor([0.2788, 0.2657, 0.2629])
-        self.input_std = torch.Tensor([0.2064, 0.1944, 0.2252])
-
-        c = 3
-        self.use_skip = use_skip
-        self.n_conv = len(layers)
-        skip_layer_size = [3] + layers[:-1]
-        for i, l in enumerate(layers):
-            self.add_module('conv%d' % i, self.Block(c, l, kernel_size, 2))
-            c = l
-        # Produce lower res output
-        for i, l in list(enumerate(layers))[::-1]:
-            self.add_module('upconv%d' % i, self.UpBlock(c, l, kernel_size, 2))
-            c = l
-            if self.use_skip:
-                c += skip_layer_size[i]
-        self.classifier = torch.nn.Conv2d(c, n_class, 1)
+      c = 3
+      self.use_skip = use_skip
+      self.n_conv = len(layers)
+      skip_layer_size = [3] + layers[:-1]
+      for i, l in enumerate(layers):
+          self.add_module('conv%d' % i, self.Block(c, l, kernel_size, 2))
+          c = l
+      for i, l in list(enumerate(layers))[::-1]:
+          self.add_module('upconv%d' % i, self.UpBlock(c, l, kernel_size, 2))
+          c = l
+          if self.use_skip:
+              c += skip_layer_size[i]
+      self.classifier = torch.nn.Conv2d(c, n_output_channels, 1)
 
     def forward(self, x):
+        #print(x.size())
         z = (x - self.input_mean[None, :, None, None].to(x.device)) / self.input_std[None, :, None, None].to(x.device)
         up_activation = []
         for i in range(self.n_conv):
             # Add all the information required for skip connections
             up_activation.append(z)
-            z = self._modules['conv%d' % i](z)
+            z = self._modules['conv%d'%i](z)
 
         for i in reversed(range(self.n_conv)):
-            z = self._modules['upconv%d' % i](z)
+            z = self._modules['upconv%d'%i](z)
             # Fix the padding
             z = z[:, :, :up_activation[i].size(2), :up_activation[i].size(3)]
             # Add the skip connection
             if self.use_skip:
                 z = torch.cat([z, up_activation[i]], dim=1)
-        soft_argmax = spatial_argmax(self.classifier(z).squeeze(1))          
-        return soft_argmax
-    
-    
+        #print(self.classifier(z).squeeze(1).size())
+        z = torch.mean(z, dim=(2, 3))        
+        z = torch.squeeze(z)
+        return spatial_argmax(self.classifier(z))
+
 
 def save_model(model):
     from torch import save
