24e8f54b1feefbbb49794a2d01eb117381180487
Baktanoosh <77557980+Baktanoosh@users.noreply.github.com>
2023-04-07 16:15:10 -0400

    Update train.py

--- homework5/homework/train.py
+++ homework5/homework/train.py
@@ -5,46 +5,40 @@ import numpy as np
 from .utils import load_data
 from . import dense_transforms
 import inspect
- 
- 
+from os import path
  
 def train(args):
-    from os import path
-    train_logger, valid_logger = None, None
-    if args.log_dir is not None:
-        train_logger = tb.SummaryWriter(path.join(args.log_dir, 'train'))
-
     global_step = 0
-    num_epoch = 50
-    learning_rate = 1e-3
-    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    model = Planner().to(device)
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
-    #det_loss = torch.nn.SmoothL1Loss()
-    det_loss = torch.nn.MSELoss()
-    transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
-    train_data = load_data('drive_data', num_workers=4, transform=transform)
-    for epoch in range(num_epoch):
-        loss_array =[]
-        model.train()
-        for img, gt_det, in train_data:
-            img, gt_det= img.to(device), gt_det.to(device)
-            size_w, _ = gt_det.max(dim=1, keepdim=True)
-            det= model(img)
-            p_det = torch.sigmoid(det * (1-2*gt_det))
-            det_loss_val = (det_loss(det, gt_det)*p_det).mean() / p_det.mean()
-            loss_val = det_loss_val
-            loss_array.append(loss_val.cpu().detach().numpy())
-            optimizer.zero_grad()
-            loss_val.backward()
-            optimizer.step()
-            global_step += 1
-        scheduler.step()
-        print("------------------------------------------------------------")
-        print("Epoch: " + str(epoch+1))
-        print("Loss: " + "{0:.4f}".format(np.mean(loss_array)))
-    save_model(model)
+num_epoch = 50
+learning_rate = 1e-3
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+model = Planner().to(device)
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.1)
+det_loss = torch.nn.MSELoss()
+transform = eval(args.transform, {k: v for k, v in inspect.getmembers(dense_transforms) if inspect.isclass(v)})
+train_data = load_data('drive_data', num_workers=4, transform=transform)
+
+for epoch in range(num_epoch):
+    epoch_loss = 0
+    model.train()
+    for img, gt_det in train_data:
+        img, gt_det = img.to(device), gt_det.to(device)
+        det = model(img)
+        p_det = torch.sigmoid(det * (1-2*gt_det))
+        det_loss_val = (det_loss(det, gt_det)*p_det).mean() / p_det.mean()
+        loss_val = det_loss_val
+        epoch_loss += loss_val.item()
+        optimizer.zero_grad()
+        loss_val.backward()
+        optimizer.step()
+        global_step += 1
+    scheduler.step()
+    print("------------------------------------------------------------")
+    print("Epoch: " + str(epoch+1))
+    print(f"Epoch {epoch+1}/{num_epoch} - Loss: {epoch_loss/len(train_data):.4f}")
+
+save_model(model)
 
 
 def log(logger, img, label, pred, global_step):
